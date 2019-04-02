1150b42f68812b30b7f45da3a31d37ebda16ab7a
sanij <shakyasanij@gmail.com>
2019-04-02 16:42:43 -0500

    the final commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+ constructor(){
+   super();
+   this.state = {
+     todos: [],
+     currentTodo : ""
+   };
+ }
+
+ onInputChange = e => {
+  this.setState({ currentTodo: e.target.value});
+ }
+
+ onClick = () => {
+   let todosCopy = this.state.todos.slice();
+   todosCopy.push(this.state.currentTodo);
+
+   this.setState({ todos: todosCopy,
+    currentTodo: ""
+  });
+
+ }
+
+ render(){
+   let bulletedTodos = this.state.todos.map((e,i)=>{
+    return( 
+      <li key= {i}>{e}</li>
     );
-  }
+   });
+   return(
+     <div>
+       <input placeholder="Enter TODO" value={this.state.currentTodo} 
+       onChange={this.onInputChange}/>
+       <button onClick={this.onClick}>Submit</button>
+       <br />
+       {this.state.todos.length === 0 ? "No Todos yet" : <ul>{bulletedTodos}</ul>}
+     </div>
+   )
+ }
 }
 
 export default App;
